71512f086c5c46e0a7e55155cde1900b5341aeeb
Ragu162004 <ragu16102004@gmail.com>
2024-10-01 22:23:45 +0530

    updated

--- src/componets/Projects/Data.jsx
+++ src/componets/Projects/Data.jsx
@@ -1,88 +1,87 @@
-import king from '../../assets/413422bb1515dc51150dd1a5b0927b6d-removebg.png';
-import tomatix from '../../assets/original-46e3d3ab-e452-488d-a218-e8064ea10ea4.webp';
-import crisper from '../../assets/crisper.png'
-import arvr from '../../assets/1723362861389.gif'
-import Work4 from '../../assets/work4.png';
-import patientimg from '../../assets/WhatsApp Image 2024-06-11 at 20.46.05_1615eed4.jpg';
-import linkup from '../../assets/linkup.png'
-import terraform from '../../assets/terraform.png'
+import king from "../../assets/413422bb1515dc51150dd1a5b0927b6d-removebg.png";
+import tomatix from "../../assets/original-46e3d3ab-e452-488d-a218-e8064ea10ea4.webp";
+import crisper from "../../assets/crisper.png";
+import arvr from "../../assets/1723362861389.gif";
+import patientimg from "../../assets/WhatsApp Image 2024-06-11 at 20.46.05_1615eed4.jpg";
+import linkup from "../../assets/linkup.png";
+import terraform from "../../assets/terraform.png";
+import car from "../../assets/carRental.png";
 
-export const ProjectData =[
+export const ProjectData = [
     {
-        id:1,
+        id: 1,
         image: tomatix,
         title: "Tomatix",
         category: "mobile",
-        link:"https://github.com/Ragu162004/Tomatix"
+        link: "https://github.com/Ragu162004/Tomatix",
     },
     {
-        id:2,
+        id: 2,
         image: arvr,
         title: "Guest House Booking - VR",
         category: "AR-VR",
-        link: "https://www.linkedin.com/posts/nikilhari-r-767568259_machinelearning-movierecommendations-streamlit-activity-7172255696355254272-XIyr?utm_source=share&utm_medium=member_desktop"
+        link: "https://www.linkedin.com/posts/nikilhari-r-767568259_machinelearning-movierecommendations-streamlit-activity-7172255696355254272-XIyr?utm_source=share&utm_medium=member_desktop",
     },
     {
-        id:3,
+        id: 3,
         image: crisper,
         title: "Crisper",
         category: "ML",
-        link:"https://www.linkedin.com/posts/ragulandiranm_excited-to-announce-my-latest-milestone-activity-7187449739934687232-n75b?utm_source=share&utm_medium=member_desktop"
+        link: "https://www.linkedin.com/posts/ragulandiranm_excited-to-announce-my-latest-milestone-activity-7187449739934687232-n75b?utm_source=share&utm_medium=member_desktop",
     },
     {
-        id:4,
+        id: 4,
         image: linkup,
         title: "LinkUp",
         category: "web",
-        link:"https://www.linkedin.com/posts/ragulandiranm_linkup-activity-7210842144247361536-XDC3?utm_source=share&utm_medium=member_desktop"
+        link: "https://www.linkedin.com/posts/ragulandiranm_linkup-activity-7210842144247361536-XDC3?utm_source=share&utm_medium=member_desktop",
     },
     {
-        id:5,
-        image: patientimg,
-        title: "Patient Records Maintanences",
-        category: "web",
-        link:"https://github.com/Ragu162004/Patients_Record"
-    },
-    {
-        id:6,
+        id: 6,
         image: king,
         title: "The King",
         category: "web",
-        link:"https://ragu162004.github.io/Restuarant_app_/"
+        link: "https://ragu162004.github.io/Restuarant_app_/",
     },
     {
-        id:7,
-        image: Work4,
-        title: "Car Rental Managaement System",
-        category: "web",
-        link:"https://github.com/Ragu162004/Car-Rental-Managament-System"
-    },
-    {
-        id:8,
+        id: 8,
         image: terraform,
         title: "Terraform AWS credential Creation",
         category: "other",
-        link:"https://github.com/Ragu162004/aws_credential_creation"
-    }
+        link: "https://github.com/Ragu162004/aws_credential_creation",
+    },
+    {
+        id: 5,
+        image: patientimg,
+        title: "Patient Records Maintanences",
+        category: "web",
+        link: "https://github.com/Ragu162004/Patients_Record",
+    },
+    {
+        id: 7,
+        image: car,
+        title: "Car Rental Managaement System",
+        category: "web",
+        link: "https://github.com/Ragu162004/Car-Rental-Managament-System",
+    },
 ];
-export const projectsNav =[
+export const projectsNav = [
     {
-        name:'all',
+        name: "all",
     },
     {
-        name :'mobile'
+        name: "mobile",
     },
     {
-        name:'web',
+        name: "web",
     },
     {
-        name:'ML',
+        name: "ML",
     },
     {
-        name: 'AR-VR',
-    }, 
+        name: "AR-VR",
+    },
     {
-        name : 'other'
-    }
+        name: "other",
+    },
 ];
-
